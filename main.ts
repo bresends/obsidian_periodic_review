1077e02aa3feab8f1c7e2143cbde14ec7b512759
bruno <bruno.resende@gmx.com>
2023-02-04 11:20:54 -0300

    Refactor: Changed Logic in the Callback

--- main.ts
+++ main.ts
@@ -1,119 +1,82 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-} from "obsidian";
+import { App, Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
 import { ResponseModal } from "responseModal";
 import { calculateNextReview } from "utils";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface Settings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: Settings = {
 	mySetting: "default",
 };
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: Settings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a great notice!");
+			"repeat",
+			"Create Periodic Review",
+			() => {
+				this.updateReviewCicle();
 			}
 		);
-		// Perform additional things with the ribbon
+
 		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: "update-frontmatter",
-			name: "Update Frontmatter Review Cicle",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				const file = this.app.workspace.getActiveFile();
-
-				if (!file) {
-					return;
-				}
-				const frontMatter =
-					this.app.metadataCache.getFileCache(file)?.frontmatter;
-
-				if (!frontMatter) {
-					const oldText = editor.getValue();
-
-					editor.setValue(
-						"---" +
-							"\ncreated_at: " +
-							new Date(file.stat.ctime).toISOString() +
-							"\nlast_review: " +
-							new Date().toISOString() +
-							"\nnext_review: " +
-							calculateNextReview(0, new Date()).toISOString() +
-							"\nreview_count: 0" +
-							"\n---\n" +
-							oldText
-					);
-
-					new Notice("Esta nota entrou no ciclo de revisões!");
-
-					return;
-				}
-
-				new ResponseModal(this.app, (result) => {
-					if (!result) {
-						return;
-					}
-					new Notice("Revisão Agendada com sucesso!");
-					app.fileManager.processFrontMatter(file, (frontMatter) => {
-						frontMatter["review_count"] += 1;
-						frontMatter["last_review"] = new Date().toISOString();
-						frontMatter["next_review"] = calculateNextReview(
-							frontMatter["review_count"],
-							new Date(frontMatter["last_review"])
-						).toISOString();
-					});
-				}).open();
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					if (!checking) {
-						new ResponseModal(this.app, (result) => {
-							if (!result) {
-								return;
-							}
-							new Notice("Revisão Agendada com sucesso!");
-						}).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			name: "Update Review Cicle",
+			callback: () => {
+				this.updateReviewCicle();
 			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
+	}
+
+	updateReviewCicle() {
+		const file = this.app.workspace.getActiveFile();
+
+		if (!file) {
+			return;
+		}
+		const frontMatter =
+			this.app.metadataCache.getFileCache(file)?.frontmatter;
+
+		if (!frontMatter || !frontMatter["last_review"]) {
+			app.fileManager.processFrontMatter(file, (frontMatter) => {
+				frontMatter["last_review"] = new Date().toISOString();
+				frontMatter["next_review"] = calculateNextReview(
+					0,
+					new Date()
+				).toISOString();
+				frontMatter["review_count"] = 0;
+			});
+
+			return new Notice("This note is now on the review cicle!");
+		}
+
+		new ResponseModal(this.app, (result) => {
+			if (!result) {
+				return;
+			}
+			new Notice("Revisão Agendada com sucesso!");
+			app.fileManager.processFrontMatter(file, (frontMatter) => {
+				frontMatter["review_count"] += 1;
+				frontMatter["last_review"] = new Date().toISOString();
+				frontMatter["next_review"] = calculateNextReview(
+					frontMatter["review_count"],
+					new Date(frontMatter["last_review"])
+				).toISOString();
+			});
+		}).open();
 	}
 
 	onunload() {
@@ -146,14 +109,14 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl("h2", { text: "Configurações de Plugin." });
+		containerEl.createEl("h2", { text: "Plugin Settings." });
 
 		new Setting(containerEl)
-			.setName("Configuração #1")
+			.setName("Config #1")
 			.setDesc("API secret")
 			.addText((text) =>
 				text
-					.setPlaceholder("Entre com a API Key")
+					.setPlaceholder("Enter your API Key")
 					.setValue(this.plugin.settings.mySetting)
 					.onChange(async (value) => {
 						console.log("Secret: " + value);
